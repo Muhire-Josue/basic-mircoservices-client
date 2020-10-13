1e8b10780503e65b0a1f089d233b608ff0414060
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-10-13 15:25:16 +0200

    create comment

--- src/CommentCreate.js
+++ src/CommentCreate.js
@@ -8,7 +8,7 @@ const CommentCreate = ({ postId }) => {
     const onSubmit = async e => {
         e.preventDefault();
 
-        axios.post(`http://localhost:4001/posts/${postId}/comments'`, {
+        axios.post(`http://localhost:4001/posts/${postId}/comments`, {
             content
         });
     }
