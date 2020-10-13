70f7907be448042784dc9b3e65a87e837df12c2b
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-10-13 13:21:21 +0200

    view posts

--- src/PostList.js
+++ src/PostList.js
@@ -12,6 +12,19 @@ const PostList = () => {
     useEffect(() => {
         fetchPosts();
     }, []);
+    const renderedPosts = Object.values(posts).map(post => {
+        return (
+            <div
+                className="card"
+                style={{ width: '30%', marginBottom: '20px' }}
+                key={post.id}
+            >
+                <div className="card-body">
+                    <h3>{post.title}</h3>
+                </div>
+            </div>
+        )
+    })
     return (
         <div>
 
