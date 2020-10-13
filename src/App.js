2777950dae89f99ed2ba99199e4a0fb044fb5c5d
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-10-13 13:00:06 +0200

    view posts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import CreatePost from './createPost';
+import PostList from './PostList';
 
 const App = () => {
     return (
         <div className="container">
             <h1>Create Post</h1>
             <CreatePost />
+            <hr />
+            <h2> Posts </h2>
+            <PostList />
         </div>
     )
 }
